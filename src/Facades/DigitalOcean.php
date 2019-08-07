56f777fbd778b19b2d7ad1a8127f18e1070da050
powerpbx <admin@powerpbx.org>
2019-08-07 15:47:15 -0700

    Update DigitalOcean.php

--- src/Facades/DigitalOcean.php
+++ src/Facades/DigitalOcean.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace GrahamCampbell\DigitalOcean\Facades;
+namespace PowerPBX\DigitalOcean\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
