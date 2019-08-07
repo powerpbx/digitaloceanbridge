e51ca4fd730dc46133cdfecc7a4ffed6b3897114
powerpbx <admin@powerpbx.org>
2019-08-07 10:30:34 -0700

    Update DigitalOceanManager.php

--- src/DigitalOceanManager.php
+++ src/DigitalOceanManager.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace GrahamCampbell\DigitalOcean;
+namespace PowerPBX\DigitalOcean;
 
 use PowerPBX\Manager\AbstractManager;
 use Illuminate\Contracts\Config\Repository;
